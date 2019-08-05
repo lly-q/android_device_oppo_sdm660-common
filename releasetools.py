94db197112da4df7270029bf8cbccd7f99f310b3
Akhil Narang <akhilnarang.1999@gmail.com>
2019-08-05 20:07:03 +0530

    sdm660-common: releasetools: Be more clear about firmware image patching
    This looks very odd on most of the devices using this tree
    because nothing is being patched. Just mention which image
    is being flashed and only when it actually is.
    
    Change-Id: Ib6c35e08aee0f1c102da28ca37434093a92f7045

--- releasetools.py
+++ releasetools.py
@@ -19,9 +19,9 @@ def AddImage(info, basename, dest):
     name = basename
     data = info.input_zip.read("IMAGES/" + basename)
     common.ZipWriteStr(info.output_zip, name, data)
+    info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
     info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-    info.script.Print("Patching firmware images...")
     AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
     return
