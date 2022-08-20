ec8fbf38017d1f2daee927eff15bff4f29b5f485
loserq <2650009628@qq.com>
2022-08-20 18:34:04 +0800

    sdm660-common: Build display stack

--- sdm660.mk
+++ sdm660.mk
@@ -63,6 +63,31 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator@1.0-service \
+    gralloc.sdm660
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-service \
+    hwcomposer.sdm660
+
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    memtrack.sdm660
+
+PRODUCT_PACKAGES += \
+    libdisplayconfig.qti \
+    libqdMetaData \
+    libtinyxml \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@2.0 \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor
+
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
