5bc83ecad9cd9717e9645dc4ff0f73a2c8dc4989
loserq <2650009628@qq.com>
2022-08-20 17:28:24 +0800

    sdm710-common: Define architecture flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,21 @@
 
 COMMON_PATH := device/oppo/sdm660-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := kryo
+
+TARGET_USES_UEFI := true
+TARGET_USES_64_BIT_BINDER := true
+
 # Inherit the proprietary files
 include vendor/oppo/sdm660-common/BoardConfigVendor.mk
