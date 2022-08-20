02cd53c09b41c307ea68e2f1531a319512e85a4f
loserq <2650009628@qq.com>
2022-08-20 17:14:08 +0800

    sdm660-common: Initial skeleton

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/oppo/sdm660-common
+
+# Inherit the proprietary files
+include vendor/oppo/sdm660-common/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,9 @@
+# Common device tree for OPPO SDM660 devices
+
+```
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
