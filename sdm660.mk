bd2b738d1bb2a1aae0d54461a8297003dcc326f8
loserq <2650009628@qq.com>
2022-08-20 20:35:27 +0800

    sdm660-common:  Build LED based vibrator HIDL

--- sdm660.mk
+++ sdm660.mk
@@ -233,6 +233,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
