aee2545a74004f94cd5527d5b2a258ec5d971787
loserq <2650009628@qq.com>
2022-08-20 17:14:08 +0800

    sdm660-common: Initial skeleton

---
+++ sdm660.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/oppo/sdm660-common/sdm660-common-vendor.mk)
