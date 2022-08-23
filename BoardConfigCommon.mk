LineageOS/android_device_motorola_sdm660-common

sdm660-common: Simplify AVB flag logic
* AVB's `--flags 3` argument builds a disabled VBMeta with
  disabled hashtree, so no need to set both flags individually.

Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838

BoardConfigCommon.mk
@@ -173,7 +173,7 @@ PRODUCT_FULL_TREBLE_OVERRIDE := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := false
-BOARD_BUILD_DISABLED_VBMETAIMAGE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true

lly-q/android_device_oppo_sdm660-common

BoardConfigCommon.mk
@@ -68,7 +68,7 @@ TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 3
 
 # Inherit the proprietary files
 include vendor/oppo/sdm660-common/BoardConfigVendor.mk